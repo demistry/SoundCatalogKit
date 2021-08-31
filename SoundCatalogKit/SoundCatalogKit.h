f75aa7a5accde3799db37fcaba13b696d0f398ed
David Ilenwabor <dave11ilen@gmail.com>
2021-08-31 06:04:15 +0100

    Initial Commit

---
+++ SoundCatalogKit/SoundCatalogKit.h
@@ -0,0 +1,18 @@
+//
+//  SoundCatalogKit.h
+//  SoundCatalogKit
+//
+//  Created by David Ilenwabor on 31/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SoundCatalogKit.
+FOUNDATION_EXPORT double SoundCatalogKitVersionNumber;
+
+//! Project version string for SoundCatalogKit.
+FOUNDATION_EXPORT const unsigned char SoundCatalogKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundCatalogKit/PublicHeader.h>
+
+
