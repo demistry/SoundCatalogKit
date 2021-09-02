11fc81e0595c55752ac9c7428276f595f4f7a44e
David Ilenwabor <dave11ilen@gmail.com>
2021-09-02 09:35:15 +0100

    Added doc comments for SHMatch

--- SoundCatalogKit/SCMatch.swift
+++ SoundCatalogKit/SCMatch.swift
@@ -8,10 +8,20 @@
 import Foundation
 import ShazamKit
 
+/// An object that represents the custom catalog media items that match a query.
+///  
+/// A single query signature may match more than one reference signature. In addition, one reference signature may map to many media items.
 public class SCMatch: NSObject {
     private var match: SHMatch
+    
+    /// An array of the media items in the catalog that match the query signature, in order of the quality of the match.
     public var mediaItems: [SCMatchedMediaItem] {
-        match.mediaItems.map(SCMatchedMediaItem.init)
+        return match.mediaItems.map(SCMatchedMediaItem.init)
+    }
+    
+    /// The query signature for the match.
+    public var querySignature: SCSignature {
+        return SCSignature(signature: match.querySignature)
     }
     
     init(match: SHMatch) {
