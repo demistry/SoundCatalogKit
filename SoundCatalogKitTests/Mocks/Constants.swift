a7f22acb3af44a5f69125afbfe5b3ce1b24c9d95
David Ilenwabor <dave11ilen@gmail.com>
2021-09-03 15:31:41 +0100

    Updated test resources

--- SoundCatalogKitTests/Mocks/Constants.swift
+++ SoundCatalogKitTests/Mocks/Constants.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class Constants {
     static let shared = Constants()
-    static let FoodMathAudioURL = Bundle(for: type(of: shared)).url(forResource: "FoodMathAudio", withExtension: "m4a")!
-    static let FoodMathAudioSignatureURL = Bundle(for: type(of: shared)).url(forResource: "AudioSignature", withExtension: "shazamsignature")!
-    static let FoodMathAudioCustomCatalogURL = Bundle(for: type(of: shared)).url(forResource: "MyCatalog", withExtension: "shazamcatalog")!
+    static let FoodMathAudioURL = Bundle(for: type(of: shared)).url(forResource: "BabyShark", withExtension: "m4a")!
+    static let FoodMathAudioSignatureURL = Bundle(for: type(of: shared)).url(forResource: "BabyShark", withExtension: "shazamsignature")!
+    static let FoodMathAudioCustomCatalogURL = Bundle(for: type(of: shared)).url(forResource: "BabyShark", withExtension: "shazamcatalog")!
     
     private init() {}
 }
