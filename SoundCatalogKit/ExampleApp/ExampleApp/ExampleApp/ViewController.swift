914d31b574b4e755f1a044caf6034074e0d28f32
David Ilenwabor <dave11ilen@gmail.com>
2021-09-03 04:34:17 +0100

    Added example app to framework

---
+++ SoundCatalogKit/ExampleApp/ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by David Ilenwabor on 03/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
